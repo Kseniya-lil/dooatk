fe9b5157be5dd1169bc45128deeb0c581c074254
Kseniya <umatrix00@mail.ru>
2021-03-31 09:55:03 +0600

    redact js

---
+++ assets/js/script.js
@@ -0,0 +1,73 @@
+//const con = 25;
+//let _a = 32;
+//let A = 24;
+//let a = 1;
+//a = 2;
+
+//console.log(a);
+
+//function(t) {
+//a = 24;
+//}
+//var b = 2;
+//let i = 0;
+////while (i < 10) {
+//a++
+//}
+
+//age: 4;
+//let n = "c"; //char
+//let v = 5; //int
+//let k = 5.6; //float
+//let y = "ttttttttt"; //string
+//object
+//let obj = {
+//   name: "vs",
+//   age: 24,
+//   hobbi: ["music", "sport"]
+//};
+//console.log(typeof(obj)); //вывод типа переменной
+
+//let mass = ['1', 25, 'hello']; //вывод по номеру
+//console.log("всем привет"); //${name}
+//let name2 = prompt("hjkjhflk");
+//console.log('fghjhfj ${name2}');
+//let agen = confirm("?");
+
+//console.log(agen);
+
+//let kr = 7;
+//let nr = 10;
+//if (kr > 8 && kr != nr || kr < 4) {
+//    console.log(true);
+//} else {
+//    console.log(false);
+//}
+
+//let il = +prompt("введите 0 - 5");
+//switch (il) {
+//  case 1:
+//    console.log("one");
+//  break;
+//case 2:
+//  console.log("two");
+//break;
+//case 3:
+//  console.log("three");
+//break;
+
+//default:
+//console.log("zero");
+//break;
+//}
+
+//let i = 0;
+//while (i < 10) {
+//   console.log("hello");
+//    i++;
+//}
+
+
+//for (let i = 0; i < 10; i++) {
+//    console.log("hello" + i);
+//}
